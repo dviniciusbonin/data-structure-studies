82eb0674270fd646dec3abeff9352545c0780298
Douglas Vinicius <vinibonin007@gmail.com>
2022-08-22 00:19:18 -0300

    feat: add bubble sort method

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
         System.out.println("Before selection sort method \n" + sort.toString());
         sort.selectionSort();
         System.out.println("After selection sort method \n" + sort.toString());
+
+        System.out.println("\n");
+        sort.setArray(generateRandomArray());
+
+        System.out.println("Before bubble sort method \n" + sort.toString());
+        sort.bubbleSort();
+        System.out.println("After bubble sort method \n" + sort.toString());
     }
 
     public static int[] generateRandomArray() {
--- src/Sort.java
+++ src/Sort.java
@@ -4,7 +4,9 @@ public class Sort {
     public Sort(int[] array) {
         this.array = array;
     }
-
+    public void setArray(int[] array) {
+        this.array = array;
+    }
     public void selectionSort() {
         for (int i=0; i < array.length - 1; i++) {
             int indexSmaller = i;
@@ -18,6 +20,18 @@ public class Sort {
                 int aux = array[i];
                 array[i] = array[indexSmaller];
                 array[indexSmaller] = aux;
+            }
+        }
+    }
+
+    public void bubbleSort() {
+        for (int i = 0; i < array.length - 1; i++) {
+            for(int j = 0; j < (array.length - i - 1); j++) {
+                if(array[j] > array[j + 1]) {
+                    int aux = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = aux;
+                }
             }
         }
     }
